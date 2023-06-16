3d339dbb6a987de30c8ac8f79e3307789dc45bcf
Jefferson Xavier <parajotaxis@gmail.com>
2023-06-16 17:47:52 -0300

    Commit passos

---
+++ app.py
@@ -0,0 +1,12 @@
+import pyautogui
+from time import sleep
+
+# passos manuais para realizar este processo?
+# 1- clicar e digitar meu usuario
+# 2- clicar e digitar minha senha
+# 3- clicar em "Entrar"
+# 4- Extrair cada produto
+#       1- clicar e digitar produto
+#       2- clicar e digitar quantidade
+#       3- clicar e digitar preço
+#       2- clicar em registrar
